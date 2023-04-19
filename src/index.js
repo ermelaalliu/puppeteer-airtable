3ff716140671c2c59e75a7a47b60571c5806d221
mela <alliuermela@gmail.com>
2023-04-20 00:33:02 +0200

    better structuring

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const dotenv = require("dotenv").config({ path: "./.env.development" });
-const puppeteer = require("puppeteer");
-const Airtable = require("airtable");
-Airtable.configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
+import { launch } from "puppeteer";
+import Airtable, { configure } from "airtable";
+
+
+configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
 
 (async () => {
-
-  const base = new Airtable({
-    endpointUrl: "https://api.airtable.com",
-    apiKey: process.env.AIRTABLE_API_TOKEN,
-  }).base(process.env.AIRTABLE_PRODUCTS_BASE_ID);
 
   const getProducts = async (page) => {
     return await page.evaluate(() => {
       const results = document.querySelectorAll(".s-item");
-      return [...results].slice(1, 5).map((item) => {
+      return [...results].slice(1, 6).map((item) => {
         return {
           name: item.querySelector(".s-item__title").innerText,
           price: item.querySelector(".s-item__price").innerText,
@@ -24,19 +21,25 @@ Airtable.configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
     }, await page.$$(".s-item"));
   };
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto("https://www.ebay.com/");
   await page.waitForSelector("#gh-ac");
   await page.type("#gh-ac", "macbook pro m1");
 
+  /* ensure that the page has finished loading and all resources have been fetched */
   await Promise.all([
     page.click("#gh-btn"),
     page.waitForNavigation({ waitUntil: "networkidle0" }),
   ]);
 
   const productsData = await getProducts(page);
+
+  const base = new Airtable({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.AIRTABLE_API_TOKEN,
+  }).base(process.env.AIRTABLE_PRODUCTS_BASE_ID);
 
   // as an argument of base() we should pass the name of the table or the id. id is better bc it never changes. one might change the name
   base(process.env.AIRTABLE_PRODUCTS_TABLE_ID).create(
@@ -46,6 +49,7 @@ Airtable.configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
         }
     */
     productsData.map((product) => {
+      /* here it is better to use ids as well */
       return { fields: { ...product } };
     }),
     function (err, records) {
@@ -53,9 +57,8 @@ Airtable.configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
         console.error(err);
         return;
       }
-      records.forEach(function (record) {
+      records.forEach( (record) => {
         console.log(record.getId());
-        return;
       });
     }
   );
