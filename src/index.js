9cc5360dfa1f21fd331a4e2e8c4d2e748da88548
mela <alliuermela@gmail.com>
2023-04-19 23:59:24 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
+const dotenv = require("dotenv").config({ path: "./.env.development" });
 const puppeteer = require("puppeteer");
 const Airtable = require("airtable");
+Airtable.configure({ apiKey: process.env.AIRTABLE_API_TOKEN });
 
 (async () => {
-  /*const getPrices = async (page) => {
 
-      await page.waitForSelector("span.s-item__price");
-    return await page.$$eval("span.s-item__price", (spans) => {
-      return [...spans]
-        .slice(1)
-        .map((span) => span.innerHTML.replace("$", "").replace(",", ""));
-    }); 
-  };*/
+  const base = new Airtable({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.AIRTABLE_API_TOKEN,
+  }).base(process.env.AIRTABLE_PRODUCTS_BASE_ID);
 
   const getProducts = async (page) => {
-    const products = await page.evaluate(() => {
+    return await page.evaluate(() => {
       const results = document.querySelectorAll(".s-item");
-      const data = [];
-      for (let i = 0; i < results.length; i++) {
-        const product = {
-          name: results[i].querySelector(".s-item__title").innerText,
-          price: results[i].querySelector(".s-item__price").innerText,
-          image: results[i].querySelector(".s-item__image-img").src,
-          description: results[i].querySelector(".s-item__subtitle").innerText,
+      return [...results].slice(1, 5).map((item) => {
+        return {
+          name: item.querySelector(".s-item__title").innerText,
+          price: item.querySelector(".s-item__price").innerText,
+          imageSrc: item.querySelector(".s-item__image-wrapper img").src,
+          description: item.querySelector(".s-item__subtitle").innerText,
         };
-        data.push(product);
-      }
-      return data;
-    });
-
-    return products;
+      });
+    }, await page.$$(".s-item"));
   };
 
   const browser = await puppeteer.launch({ headless: false });
@@ -37,12 +30,34 @@ const Airtable = require("airtable");
   await page.goto("https://www.ebay.com/");
   await page.waitForSelector("#gh-ac");
   await page.type("#gh-ac", "macbook pro m1");
-  await page.click("#gh-btn");
 
-  /* console.log(await getPrices(page)); */
-  console.log(await getProducts(page));
+  await Promise.all([
+    page.click("#gh-btn"),
+    page.waitForNavigation({ waitUntil: "networkidle0" }),
+  ]);
 
-  await page.click("a.pagination__next");
-  /*  console.log(await getPrices(page)); */
-  console.log(await getProducts(page));
+  const productsData = await getProducts(page);
+
+  // as an argument of base() we should pass the name of the table or the id. id is better bc it never changes. one might change the name
+  base(process.env.AIRTABLE_PRODUCTS_TABLE_ID).create(
+    /* each product item should have a structure like this acc to docs
+         {
+            "fields": {}
+        }
+    */
+    productsData.map((product) => {
+      return { fields: { ...product } };
+    }),
+    function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record) {
+        console.log(record.getId());
+        return;
+      });
+    }
+  );
+  await browser.close();
 })();
